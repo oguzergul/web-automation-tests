c1c708aef1d5477aa313592806ddb549fc850083
oguzergul <erguloguz@hotmail.com>
2021-11-21 11:01:07 +0300

    [feat] sign in scenario and steps has been successfully added.

--- src/test/java/definitions/step_definitions.java
+++ src/test/java/definitions/step_definitions.java
@@ -6,10 +6,12 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.HomePage;
 import pages.ResultPage;
+import pages.SignInPage;
 
 public class step_definitions {
     HomePage homePage = new HomePage();
     ResultPage resultPage = new ResultPage();
+    SignInPage signInPage = new SignInPage();
 
     @Given("I open BestBuy homepage")
     public void homePageIsOpen(){
@@ -39,5 +41,30 @@ public class step_definitions {
     @Then("I check search results have been listed")
     public void checkForResults() {
         resultPage.verifySearchResults();
+    }
+
+    @When("I open {string} dropdown")
+    public void openDropdown(String name) {
+        homePage.openDropdown(name);
+    }
+
+    @And("I choose {string} submenu")
+    public void openSubMenu(String name) {
+        homePage.openSubMenu(name);
+    }
+
+    @When("I check Sign In Page title is {string}")
+    public void verifyTitle(String title) {
+        signInPage.verifyTitle(title);
+    }
+
+    @When("I fill Sign In form with {string} and {string}")
+    public void fillSignInForm(String email, String password) {
+        signInPage.fillSignInForm(email, password);
+    }
+
+    @When("I check if the login was successful")
+    public void checkLoginIsSuccessful() {
+        signInPage.checkLoginIsSuccessful();
     }
 }
--- src/test/resources/BestBuy.feature
+++ src/test/resources/BestBuy.feature
@@ -3,8 +3,16 @@ Feature: BestBuy Web Automation Tests
     Given I open BestBuy homepage
     And I close first opening popup
 
-  Scenario: search result test
+  Scenario: Search Result Test
     And I maximize window
     When I check homepage meta title
     When I search for "drone" on search-box
     Then I check search results have been listed
+
+  Scenario: Login with Email and Password
+    When I open "Account" dropdown
+    And I choose "Account" submenu
+    When I check Sign In Page title is "Sign In to Best Buy"
+    When I fill Sign In form with "oguzergul98@gmail.com" and "OGuz98.5ouz"
+    When I check if the login was successful
+
